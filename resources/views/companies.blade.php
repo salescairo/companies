1afefe9c4d0cea95682432304606abbc1f82bd8a
Cairo Sales <cairoosf@gmail.com>
2022-06-13 13:06:55 -0300

    personalizar visualização das empresas

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -5,15 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Companies</title>
+    <style>
+        .d-center{
+            display: flex;
+            justify-content: center;
+        }
+        td{
+            border: 1px solid #343434;
+            padding: 4px 4px 4px 4px;
+        }
+        tr{
+            margin-bottom: 12px;
+        }
+        .grey-5{
+            background-color: #CDCDCD;
+
+        }
+
+    </style>
 </head>
 <body>
     <h1>Companies</h1>
     <hr>
     <p>
     {{ number_format(count($models),2,'','.')." registros" }}
-    <table>
+    <table class="d-center">
     @foreach($models as $model)
-    <tr>
+    <tr class="grey-5">
         <td>{{ $model->id }}</td>
         <td>{{ $model->name }}</td>
         <td>{{ $model->fantasy_name }}</td>
